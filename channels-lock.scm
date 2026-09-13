cf220e930c60f7aa205176d30185dc0e4209dc2d
João Victor Gomes Izidoro <joaovictor.gizidoro@gmail.com>
2026-09-13 20:40:31 -0300

    chore: create channels and channel-lock

---
+++ channels-lock.scm
@@ -0,0 +1,10 @@
+(list (channel
+       (name 'guix)
+       (url "https://git.guix.gnu.org/guix.git")
+       (branch "master")
+       (commit "d4c93bc1c373776b671ae5a940e2272c2a6f58fa")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
